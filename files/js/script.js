7dde6e1bf455687c51a4cd734d082f9946395b41
Jim Sayer <jls1153@comcast.net>
2023-07-27 09:18:26 -0400

    Update script.js

--- files/js/script.js
+++ files/js/script.js
@@ -1,15 +1,19 @@
 // Listener to toggle sidebar and section
 var hamburger = document.querySelector(".hamburger");
-var sidebar = document.querySelector(".sidebar");
-var officersId = document.getElementById("officers");
 hamburger.addEventListener("click", function(){
 	document.querySelector("body").classList.toggle("active");
     console.log(hamburger);
     console.log(officersId);
     console.log(sidebar);
 })
-officersId.addEventListener("click", function(){
 var sectionCont = document.querySelector(".container");
-    console.log(sectionCont);
+console.log(sectionCont);
+
+var officersId = document.getElementById("officers");
+officersId.addEventListener("click", function(){
     sectionCont.textContent = "This is the officers page."
+})
+var homeId = document.getElementById("home");
+homeId.addEventListener("click", function(){
+    sectionCont.textContent = "This is the home page."
 })
