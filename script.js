ec49a6c9c2e687e64f970fe2a0ef2ad9e4699430
Jim Sayer <jls1153@comcast.net>
2023-07-28 07:59:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Listener to toggle sidebar and section
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function(){
+	document.querySelector("body").classList.toggle("active");
+    console.log(hamburger);
+    console.log(officersId);
+    console.log(sidebar);
+})
+var officersId = document.getElementById("officers");
+var sectionCont = document.querySelector(".container");
+console.log(sectionCont);
+officersId.addEventListener("click", function(){
+    sectionCont.textContent = "This is the officers page."
+})
+var homeId = document.getElementById("home");
+homeId.addEventListener("click", function(){
+    sectionCont.textContent = "This is the home page."
+})
