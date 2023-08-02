02d516c6bd3810793fdf21cb444d2a8296bb2421
Jim Sayer <jls1153@comcast.net>
2023-08-02 18:25:49 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Initialize the Home Page
+   var homeHtml = `<h1><center>Welcome to Council 13026</center></h1>
+    <p><center><i>Serving Saint Francis Parish and the Litchfield NH Community</center></i></p>`;
+    document.getElementById("display").innerHTML = (homeHtml);
+
 // Listener to toggle sidebar and section
 var sidebar = document.querySelector(".sidebar");
 var hamburger = document.querySelector(".hamburger");
@@ -8,8 +13,8 @@ hamburger.addEventListener("click", function(){
     console.log(sidebar);
 })
 var officersId = document.getElementById("officers");
-var sectionCont = document.querySelector(".container");
-console.log(sectionCont);
+//var sectionCont = document.querySelector(".container");
+//console.log(sectionCont);
 officersId.addEventListener("click", function(){
     // Get the container element
 //var sidebarContainer = document.getElementById("sidebar");
@@ -23,21 +28,16 @@ officersId.addEventListener("click", function(){
     //console.log(current);
     //var options = document.getElementsByClassName("options");
     //console.log(options);
-    sectionCont.textContent = "This is the officers page."
+    var OfficersHtml = `<h1><center>2023-2024 Council Officers</center></h1>;
+    document.getElementById("display").innerHTML = (OfficersHtml);
+    //sectionCont.textContent = "This is the officers page."
 })
 
 var homeId = document.getElementById("home");
 homeId.addEventListener("click", function(){
-    console.log(homeId);
-    var homeHtml = `<h1><center>Welcome to Council 13026</center></h1>
-    <p><center><i>Serving Saint Francis Parish and the Litchfield NH Community</center></i></p>`;
+ //   var homeHtml = `<h1><center>Welcome to Council 13026</center></h1>
+  //  <p><center><i>Serving Saint Francis Parish and the Litchfield NH Community</center></i></p>`;
     //var current = document.getElementsByClassName("active");
     //console.log(current);
-
-    //sectionCont.textContent = (homeHtml);
-
     document.getElementById("display").innerHTML = (homeHtml);
-    //const homeText = document.querySelector("#homeText").innerHTML;
-    //console.log(homeText);
-    //container.innerHTML = homeText;
 })
