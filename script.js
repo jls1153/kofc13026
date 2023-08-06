ba105750b818e7e4b419cec0cf9a8f8f8dc65b85
Jim Sayer <jls1153@comcast.net>
2023-08-06 09:23:18 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const section3 = document.getElementById('section3');
 const hamburger = document.querySelector(".hamburger");
 const homeId = document.getElementById("home");
 const membershipId = document.getElementById("membership");
+const officersId = document.getElementById("officers");
+const activitiesId = document.getElementById("activities");
+const insuranceId = document.getElementById("insurance");
+
 window.addEventListener('load', function() {
   showSection(section1);
 });
@@ -29,46 +33,17 @@ membershipId.addEventListener("click", function(){
     showSection(section2);
 })
 // Listener for Officers Selection
-var officersId = document.getElementById("officers");
 officersId.addEventListener("click", function(){
-    var officersHtml = `<h1><center>2023-2024 Council Officers</center></h1>`;
-    document.getElementById("display").innerHTML = (officersHtml);
+    showSection(section3);
 })
 // Listener for Activities Selection
-var activitiesId = document.getElementById("activities");
+
 activitiesId.addEventListener("click", function(){
-    var activitiesHtml = `<h1><center>Activities</center></h1>`;
-    document.getElementById("display").innerHTML = (activitiesHtml);
+    showSection(section4);
 })    
 // Listener for Insurance Selection
-var insuranceId = document.getElementById("insurance");
 insuranceId.addEventListener("click", function(){
-    var insuranceHtml = `<h1><center>Insurance</center></h1>
-        <br>
-        <p>Guided by our Catholic faith, we are on a mission to help expand
-        your financial security at every step of your family's journey.</p>
-        <br>
-        <div class="insOptions">
-            <ul>
-                <li>Life Insurance</li>
-                <li>Disability Income Insurance</li>
-                <li>Long-Term Care Insurance</li>
-                <li>Retirement Annuities</li>
-                <li>And more...</li> 
-                </ul>
-        </div>
-        <br>
-        <p>Call your local agent today!</p>
-        <br>
-        <div class=<insContact">
-            <ul>
-                <li>Kevin Brown</li>
-                <li>Field Agent</li>
-                <li>Office: (603)305-0204</li>
-                <li>kevin.brown@kofc.org</li>
-            </ul>
-        </div>`;
-    document.getElementById("display").innerHTML = (insuranceHtml);
+     showSection(section5);
 })
 // Listener for Reports Selection
 var reportsId = document.getElementById("reports");
