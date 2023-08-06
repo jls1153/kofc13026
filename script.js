e48a292a0129abeb2e98e4a1ec13b0ecbedd8804
Jim Sayer <jls1153@comcast.net>
2023-08-06 09:28:09 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function showSection(section) {
   section1.style.display = 'none';
   section2.style.display = 'none';
   section3.style.display = 'none';
+  section4.style.display = 'none';
+  section5.style.display = 'none';
   section.style.display = 'block';
 }
 
