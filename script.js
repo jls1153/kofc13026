85b0dc8a4b95b4d0fc453b34b15de8619ab56e0a
Jim Sayer <jls1153@comcast.net>
2023-08-09 07:06:37 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-// New code
+// Set up constants
 const section1 = document.getElementById('section1');
 const section2 = document.getElementById('section2');
 const section3 = document.getElementById('section3');
+const section4 = document.getElementById('section4');
+const section5 = document.getElementById('section5');
+const section6 = document.getElementById('section6');
+const section7 = document.getElementById('section7');
 const hamburger = document.querySelector(".hamburger");
 const homeId = document.getElementById("home");
 const membershipId = document.getElementById("membership");
 const officersId = document.getElementById("officers");
 const activitiesId = document.getElementById("activities");
 const insuranceId = document.getElementById("insurance");
+const reportsId = document.getElementById("reports");
+const linksId = document.getElementById("links");
 
+// Initialize section to home
 window.addEventListener('load', function() {
   showSection(section1);
 });
+
 function showSection(section) {
   section1.style.display = 'none';
   section2.style.display = 'none';
   section3.style.display = 'none';
   section4.style.display = 'none';
   section5.style.display = 'none';
+  section6.style.display = 'none';
+  section7.style.display = 'none';
   section.style.display = 'block';
 }
 
@@ -48,14 +58,10 @@ insuranceId.addEventListener("click", function(){
      showSection(section5);
 })
 // Listener for Reports Selection
-var reportsId = document.getElementById("reports");
 reportsId.addEventListener("click", function(){
-    var reportsHtml = `<h1><center>Reports</center></h1>`;
-    document.getElementById("display").innerHTML = (reportsHtml);
+     showSection(section6);
 })
 // Listener for Links Selection
-var linksId = document.getElementById("links");
 linksId.addEventListener("click", function(){
-    var linksHtml = `<h1><center>Links</center></h1>`;
-    document.getElementById("display").innerHTML = (linksHtml);
+    showSection(section7);
 })
