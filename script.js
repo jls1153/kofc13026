dbb724be14d8ed734944a95050f486a9d806ebe2
Jim Sayer <jls1153@comcast.net>
2023-08-22 07:29:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const section4 = document.getElementById('section4');
 const section5 = document.getElementById('section5');
 const section6 = document.getElementById('section6');
 const section7 = document.getElementById('section7');
+const section8 = document.getElementById('section8');
 const hamburger = document.querySelector(".hamburger");
 const homeId = document.getElementById("home");
 const membershipId = document.getElementById("membership");
 const officersId = document.getElementById("officers");
+const directorsId = document.getElementById("directors");
 const activitiesId = document.getElementById("activities");
 const insuranceId = document.getElementById("insurance");
 const reportsId = document.getElementById("reports");
@@ -28,6 +30,7 @@ function showSection(section) {
   section5.style.display = 'none';
   section6.style.display = 'none';
   section7.style.display = 'none';
+  section8.style.display = 'none';
   section.style.display = 'block';
 };
 
@@ -48,19 +51,23 @@ membershipId.addEventListener("click", function(){
 officersId.addEventListener("click", function(){
     showSection(section3);
 });
+// Listener for Directors Selection
+directorsId.addEventListener("click", function(){
+    showSection(section4);
+});
 // Listener for Activities Selection
 activitiesId.addEventListener("click", function(){
-    showSection(section4);
+    showSection(section5);
 });   
 // Listener for Insurance Selection
 insuranceId.addEventListener("click", function(){
-     showSection(section5);
+     showSection(section6);
 });
 // Listener for Reports Selection
 reportsId.addEventListener("click", function(){
-     showSection(section6);
+     showSection(section7);
 });
 // Listener for Links Selection
 linksId.addEventListener("click", function(){
-    showSection(section7);
+    showSection(section8);
 });
